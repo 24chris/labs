79805ecc52db42b69f5f701dab143bdfc058ad3e
24chris <40150356+24chris@users.noreply.github.com>
2023-08-27 13:13:56 +0300

    Update settings

--- fieldsimp/settings.py
+++ fieldsimp/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
-    'django_rest_passwordreset',
+    # 'django_rest_passwordreset',
     # 'djoser',
     'users',
     'students',
