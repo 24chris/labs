e1ed5c4e4a784e0890709bc5e5798638e80c3fde
24chris <40150356+24chris@users.noreply.github.com>
2023-08-27 19:06:00 +0300

    Update static files

--- fieldsimp/settings.py
+++ fieldsimp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -214,6 +215,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media/'
+
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+
+STATICFILES_DIRS = 'static'
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 
 #Email settings
