de1b02bd308ccfbb531b6766a0c5333e4e917d63
24chris <40150356+24chris@users.noreply.github.com>
2023-08-27 19:19:42 +0300

    sample rollback

--- fieldsimp/settings.py
+++ fieldsimp/settings.py
@@ -219,7 +219,7 @@ MEDIA_ROOT = BASE_DIR/'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 STATICFILES_DIRS = [BASE_DIR/'media/']
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = BASE_DIR/'media'
 
 
 #Email settings
